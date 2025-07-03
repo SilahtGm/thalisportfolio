7654a8fbe1140d5f8df36666721801082b02d139
SilahtGm <tatagamer677@gmail.com>
2025-07-02 23:55:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,8 +56,7 @@ const projetos = [
     descricao: `Olá a todos, este meu projeto foi desenvolvido durante minhas aulas na ETEC no módulo de Desenvolvimento de Sistemas.
  Utilizei a linguagem Java e a estrutura switch case para criar uma lógica que informa ao usuário a data correta para pagamento do IPVA de acordo com o final da placa do veículo.<br>`
     imagens: [
-      "./img/NovoProjeto1.png",
-      "./img/NovoProjeto2.png"
+      "./img/javaagd7.png"
     ]
   }
 ];
